fadce3a955529f81c6d685d4a043a99595d3153d
Charleston Gilliland <clstlmusic@gmail.com>
2019-10-01 08:06:21 -0500

    project

---
+++ ChallengesWithTestsMark8/Debug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChallengesWithTestsMark8
+{
+    internal class Debug
+    {
+        internal static void Log(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
